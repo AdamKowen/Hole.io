f48458d9abba9ee48de537e6d68301b0f2744528
Alon Bilman <bilman.alon@gmail.com>
2025-09-30 22:14:38 +0300

    Add minimap

---
+++ Assets/MinimapSimpleURP.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(Camera))]
+public class MinimapSimpleURP : MonoBehaviour
+{
+    [Header("Zoom (Orthographic Size)")]
+    [Tooltip("Smaller = closer")]
+    public float zoom = 30f;
+
+    Camera cam;
+
+    void OnEnable()
+    {
+        cam = GetComponent<Camera>();
+        cam.orthographic = true;
+    }
+
+    void Update()
+    {
+        if (!cam) cam = GetComponent<Camera>();
+        cam.orthographic = true;
+        cam.orthographicSize = Mathf.Max(0.001f, zoom);
+
+        // Always look straight down (north-up)
+        transform.rotation = Quaternion.identity;
+    }
+}
